13f838ba4021314bbb916ce947e7ee0d567a52d7
Rupesh kumar <rupesh18123143@gmail.com>
2025-02-14 23:41:21 +0530

    adding header

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import './App.css'
 import TodoList from './todo/TodoList'
 import Insert_todo from './todo/Insert_todo'
+import Header from './header/Header'
 
 function App() {
 
   return (
     <>
+    <Header/>
      <Insert_todo/>
      {/* <TodoList/> */}
     </>
---
+++ src/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./head.css"
+
+function Header() {
+  return (
+    <div className='card1'>
+        <h2>TO DO APP</h2>
+    </div>
+  )
+}
+
+export default Header
